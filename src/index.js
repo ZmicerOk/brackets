a3ca7ad09eaa35284107341bd786ba3c701494e1
ZmicerOk <vasikou73@gmail.com>
2019-03-24 20:59:14 +0300

    feat: function complete

--- src/index.js
+++ src/index.js
@@ -1,41 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 > 0) {
+    return false;
+  }
 
-checkTerri(0,str.length,str,bracketsConfig)
+  let configArray = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    configArray.push(bracketsConfig[i].join(""));
+  }
 
+  let tryout = 1;
 
-}
-
-
-// checkTerri = function (l, r, str, bracketsConfig) {
-//   r = str.length;
-//   l = 0;
-//   let resm = true;
-//   if (!checkCondInArr(str[l], str[r], bracketsConfig)) {
-//     //  return false
-//   while((r>l)&&(!checkCondInArr(l+1,r,str.substr(l+1,r-l),bracketsConfig))){
-//     r--;
-//   }
-//   if(!checkCondInArr(l+1,r,str.substr(l+1,r-l),bracketsConfig))
-//   {
-//     resm=false; 
-//   }
-
-//   } else {
-//     if (substr(l + 1, str.length - 1).length > 0) {
-//       checkTerri(l + 1, r - 1,str.substr(l + 1, str.length - 1),bracketsConfig);
-//     }
-//   }
-//   return resm;
-// };
-
-// checkCondInArr = function (arg1, arg2, bracketsConfig) {
-//   let res = false;
-//   for(let tempi=0;tempi<bracketsConfig.length;tempi++) {
-    
-//     if ((bracketsConfig[tempi][0] = arg1) && (bracketsConfig[tempi][2] = arg2)) {
-//       res = true;
-//     }
-//   }
-//   return res;
-// };
+  do {
+    for (let j = 0; j < configArray.length; j++) {
+      if (str.includes(configArray[j])) {
+        str = str.replace(configArray[j], "");
+        tryout++;
+      }
+    }
+    tryout--;
+  } while (tryout);
+  return str.length == 0;
+};
