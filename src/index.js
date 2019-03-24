faecc2edd274f33b3a18fadab0828b12dd390692
ZmicerOk <vasikou73@gmail.com>
2019-03-24 10:48:28 +0300

    feat: comments old code

--- src/index.js
+++ src/index.js
@@ -7,35 +7,35 @@ checkTerri(0,str.length,str,bracketsConfig)
 }
 
 
-checkTerri = function (l, r, str, bracketsConfig) {
-  r = str.length;
-  l = 0;
-  let resm = true;
-  if (!checkCondInArr(str[l], str[r], bracketsConfig)) {
-    //  return false
-  while((r>l)&&(!checkCondInArr(l+1,r,str.substr(l+1,r-l),bracketsConfig))){
-    r--;
-  }
-  if(!checkCondInArr(l+1,r,str.substr(l+1,r-l),bracketsConfig))
-  {
-    resm=false; 
-  }
+// checkTerri = function (l, r, str, bracketsConfig) {
+//   r = str.length;
+//   l = 0;
+//   let resm = true;
+//   if (!checkCondInArr(str[l], str[r], bracketsConfig)) {
+//     //  return false
+//   while((r>l)&&(!checkCondInArr(l+1,r,str.substr(l+1,r-l),bracketsConfig))){
+//     r--;
+//   }
+//   if(!checkCondInArr(l+1,r,str.substr(l+1,r-l),bracketsConfig))
+//   {
+//     resm=false; 
+//   }
 
-  } else {
-    if (substr(l + 1, str.length - 1).length > 0) {
-      checkTerri(l + 1, r - 1,str.substr(l + 1, str.length - 1),bracketsConfig);
-    }
-  }
-  return resm;
-};
+//   } else {
+//     if (substr(l + 1, str.length - 1).length > 0) {
+//       checkTerri(l + 1, r - 1,str.substr(l + 1, str.length - 1),bracketsConfig);
+//     }
+//   }
+//   return resm;
+// };
 
-checkCondInArr = function (arg1, arg2, bracketsConfig) {
-  let res = false;
-  for(let tempi=0;tempi<bracketsConfig.length;tempi++) {
+// checkCondInArr = function (arg1, arg2, bracketsConfig) {
+//   let res = false;
+//   for(let tempi=0;tempi<bracketsConfig.length;tempi++) {
     
-    if ((bracketsConfig[tempi][0] = arg1) && (bracketsConfig[tempi][2] = arg2)) {
-      res = true;
-    }
-  }
-  return res;
-};
+//     if ((bracketsConfig[tempi][0] = arg1) && (bracketsConfig[tempi][2] = arg2)) {
+//       res = true;
+//     }
+//   }
+//   return res;
+// };
